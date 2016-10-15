d855f39ac8e62f037a540b62e2db658877b73849
Samantha Bretous <sam@samanthabretous.com>
2016-10-15 17:34:28 -0400

    set up router. started styling

---
+++ src/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HelpButton from './components/HelpButton'
+
+const HomePage = React.createClass({
+  render() {
+    return (
+      <div className="homePage">
+        <HelpButton />
+      </div>
+    )
+  }
+})
+
+export default HomePage
